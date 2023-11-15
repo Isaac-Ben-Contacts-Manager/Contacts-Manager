c2738f040edd94b3b004d3fa4755a3489816d71b
Ben Heller <benheller1016@gmail.com>
2023-11-15 14:18:47 -0600

    Initial commit. Framework built.

---
+++ ContactUtils.java
@@ -0,0 +1,42 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+
+public class ContactUtils {
+
+    private HashMap<String, Contact> contactList;
+    Path filePath;
+
+    public ContactUtils (String fileName) {
+        filePath = Paths.get(fileName);
+    }
+
+    public List<String> readFile() {
+        return null;
+    }
+
+    public void writeFile (List<String> updatedContacts) {
+
+    }
+
+    public void addContact (String newContact) {
+
+    }
+
+    public void delContact (String name) {
+
+    }
+
+    public void modContact (String name) {
+
+    }
+
+    public void displayContacts() {
+
+    }
+
+    public Contact getContact(String name) {
+        return contactList.get(name);
+    }
+}
---
+++ ContactsManager.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ContactsManager {
+
+    public static void main(String[] args) {
+
+        String PATH_TO_FILE = "contacts.txt";
+        Scanner scanner = new Scanner(System.in);
+        String userResponse;
+        boolean done = false;
+
+        ContactUtils utils = new ContactUtils(PATH_TO_FILE);
+
+        // Display menu with choices
+        while (!done) {
+            System.out.println("Welcome to the Contacts Manager!\n");
+            System.out.println("1. View contacts.");
+            System.out.println("2. Add a new contact.");
+            System.out.println("3. Search a contact by name.");
+            System.out.println("4. Delete an existing contact.");
+            System.out.println("5. Exit.");
+            System.out.println("Enter an option (1-5).");
+
+            userResponse = scanner.nextLine();
+            switch (userResponse) {
+                case "1":
+                    utils.displayContacts();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Invalid response. Try again.");
+            }
+        }
+    }
+}
